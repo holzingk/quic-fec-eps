3928a38bb6a99397e376a26540509e787f078e10
Kilian Holzinger <kilian.holzinger@gmail.com>
2024-11-23 18:01:05 +0100

    Improve benchmarks

--- quiche/benches/fec.rs
+++ quiche/benches/fec.rs
@@ -4,7 +4,8 @@ use criterion::{
     criterion_main,
     Criterion,
     BenchmarkId,
-    Throughput,	
+    Throughput,
+    BatchSize,
 };
 use quiche::fec::{
     Tetrys,
@@ -16,6 +17,7 @@ enum SendSymbol {
     Source((u64, Vec<u8>)),
     Repair(RepairSymbol),
 }
+use std::time::Instant;
 
 
 fn gen_random_vec(min_len: usize, max_len: usize) -> Vec<u8> {
@@ -28,7 +30,7 @@ fn gen_random_vec(min_len: usize, max_len: usize) -> Vec<u8> {
     res
 }
 
-fn tetrys_send_symbols(num: usize, count_loss: usize) {
+fn prepare_decoder(num: usize, count_loss: usize) -> (Tetrys, Vec<Vec<u8>>){
     assert!(num > count_loss);
     let mut fec = Tetrys::new(1500).unwrap();
     let mut ss = Vec::new();
@@ -69,23 +71,59 @@ fn tetrys_send_symbols(num: usize, count_loss: usize) {
             },
         }
     }
+    (fec, missing_sids)
+}
 
+fn tetrys_decode(fec: &mut Tetrys, missing_sids: &Vec<Vec<u8>>) {
     let decoded = fec.decoder.try_decode();
     for (missing_ss,  decoded_ss) in missing_sids.iter().zip(decoded.iter()) {
 	assert_eq!(missing_ss, decoded_ss);
     }
 }
 
-fn bench_matrix_size(c: &mut Criterion) {
+fn prepare_encoder(num: usize) -> Tetrys {
+    let mut fec = Tetrys::new(1500).unwrap();
+    let mut ss = Vec::new();
+    for _i in 0..num {
+	ss.push(gen_random_vec(500, 1500));
+    }
+    for s in ss {
+	let _sid = fec.encoder.add_source_symbol(s.as_slice()).unwrap();
+    }
+    fec
+}
+
+fn tetrys_encode_symbols(fec: &mut Tetrys) {
+    let _rs = fec.encoder.generate_repair_symbol().unwrap();
+}
+
+fn bench_decoder_matrix_size(c: &mut Criterion) {
     let mut group = c.benchmark_group("decoder_matrix");
-    for size in (50..250).step_by(50) {
+    for size in (10_usize..2500_usize).step_by(10) {
+	// about 5 percent loss
+	let loss = size.div_ceil(20_usize);
 	group.throughput(Throughput::Elements(size as u64));
 	group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
-	    b.iter(|| tetrys_send_symbols(size, size - 5));
+	    b.iter_batched_ref(|| prepare_decoder(size, loss),
+			       |(ref mut fec,  missing_sids)| tetrys_decode(fec, &missing_sids),
+			       BatchSize::SmallInput)
 	});
     }
     group.finish();
 }
 
-criterion_group!(benches, bench_matrix_size);
+fn bench_encoder_matrix_size(c: &mut Criterion) {
+    let mut group = c.benchmark_group("encoder_matrix");
+    for size in (10..2500).step_by(10) {
+	group.throughput(Throughput::Elements(size as u64));
+	group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
+	    b.iter_batched_ref(|| prepare_encoder(size),
+			       |mut fec| tetrys_encode_symbols(&mut fec),
+			       BatchSize::SmallInput)
+	});
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_decoder_matrix_size, bench_encoder_matrix_size);
 criterion_main!(benches);
